43a003c66dab0e462c1709298d7a93b2801b3658
Jonn Spencer <jonn@jonnjonnjonn.com>
2021-01-04 20:59:44 +0900

    Add character grid

---
+++ onetwocb-components/projects/onetwocb-components/src/lib/character-grid/character-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-character-grid',
+  templateUrl: './character-grid.component.html',
+  styleUrls: ['./character-grid.component.css']
+})
+export class CharacterGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- onetwocb-components/projects/onetwocb-components/src/lib/onetwocb-components.module.ts
+++ onetwocb-components/projects/onetwocb-components/src/lib/onetwocb-components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { OnetwocbComponentsComponent } from './onetwocb-components.component';
+import { CharacterGridComponent } from './character-grid/character-grid.component';
 
 
 
 @NgModule({
-  declarations: [OnetwocbComponentsComponent],
+  declarations: [OnetwocbComponentsComponent, CharacterGridComponent],
   imports: [
   ],
   exports: [OnetwocbComponentsComponent]
